47865a58a61f437f98712e91e08d59e39a5b14f8
Pau Ferrer Ocaña <crazyserver@gmail.com>
2016-08-18 20:49:03 +0200

    Add filemanager

---
+++ filemanager.js
@@ -0,0 +1,35 @@
+function filemanager_openFileInfo(fileid, filename, owner, context, filearea, component, filesize, contenthash, pathnamehash, filepath, mimetype, timemodified) {
+    $('#fileModal_fileid').text(fileid);
+    $('#fileModal_filename').text(filename);
+    $('#fileModal_owner').text(owner);
+    $('#fileModal_context').text(context);
+    $('#fileModal_filearea').text(filearea);
+    $('#fileModal_component').text(component);
+    $('#fileModal_filesize').text(filesize);
+    $('#fileModal_contenthash').text(contenthash);
+    $('#fileModal_pathnamehash').text(pathnamehash);
+    $('#fileModal_filepath').text(filepath);
+    $('#fileModal_mimetype').text(mimetype);
+    $('#fileModal_timemodified').text(timemodified);
+    var href = $('#fileModal_similar').attr('original-href');
+    $('#fileModal_similar').attr('href', href + contenthash);
+}
+
+function filemanager_deleteFile() {
+    var filename = $('#fileModal_filename').text();
+    $('#deleteModal_filename').text(filename);
+}
+
+function filemanager_deleteFile_confirm() {
+    return filemanager_request('delete');
+}
+
+function filemanager_downloadFile() {
+    return filemanager_request('download');
+}
+
+function filemanager_request(action) {
+    var fileid = $('#fileModal_fileid').text();
+    var wwwroot = $('#fileModal_urlroot').text();
+    window.location = wwwroot + '/report/coursequotas/filemanager_action.php?action='+action+'&fileid='+fileid;
+}
